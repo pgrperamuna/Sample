8602ffd83de09fe1011543cf19c5177fd2b1646e
pgrperamuna <keranga297@gmail.com>
2023-01-30 13:56:12 +0530

    create php
    this is first php file

---
+++ hg.php
@@ -0,0 +1,12 @@
+//create php connection?
+$conn = new mysqli($servername, $username, $password, $database);
+
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+
+
+
+undefinedSource: https://stackoverflow.com/questions/55143102
+
+
